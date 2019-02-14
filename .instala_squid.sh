e23c871f3f2cb2d5e11c2598af58a62d3545e401
danimrprofe <danimrprofe@gmail.com>
2019-02-14 08:59:42 +0100

    Create .instala_squid.sh

---
+++ .instala_squid.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+config_folder=/etc/squid
+
+if [ -z $1 ]
+then
+	echo "Uso del comando:"
+	echo "---------------"
+	echo "instala_squid.sh"
+	echo "Ejemplos:"
+  echo "sh instala_squid"
+  echo "./instala_squid.sh"
+else
+  
+  # Instalacion de squid
+  
+  read -p "instalación de squid" parada
+
+  sudo apt $silent install squid -y
+    
+  # Borramos configuraciones anteriores
+
+  if [[ ! -d $config_folder ]]; then
+      mkdir $config_folder
+  else
+      rm -rf $config_folder/*
+  fi
+
+  cp -R * $config_folder
+ 
+ # Reiniciar servicios
+ 
+  read -p "reinicio de servicios" parada
+  sudo systemctl restart squid
+  
+ 
+fi
+
+
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
+Press h to open a hovercard with more details.
